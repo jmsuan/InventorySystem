4d285d52d6b97545afd87a7e13ea69871c3d044c
Jonathan Lane <lane.jonathan@protonmail.com>
2023-04-06 01:39:10 -0700

    Added FXML files and their respective controllers for each window/scene. Removed default files. Gave package controllers access to javafx.fxml and opened them to the module as well. Inserted basic initialization statements for each controller.

---
+++ src/main/java/lanej/inventorysystem/controller/ModifyProduct.java
@@ -0,0 +1,13 @@
+package lanej.inventorysystem.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ModifyProduct implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("ModifyProduct Controller Initialized");
+    }
+}
